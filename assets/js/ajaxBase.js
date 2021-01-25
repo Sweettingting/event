34da578d0c04c732708011d3488837d9dadb804f
hucongcong <flycc258@163.com>
2021-01-25 12:28:42 +0800

    完成了

--- assets/js/ajaxBase.js
+++ assets/js/ajaxBase.js
@@ -1,2 +1,23 @@
 //设置跟路径
 axios.defaults.baseURL = "http://api-breakingnews-web.itheima.net";
+//添加请求拦截
+axios.interceptors.request.use(
+  function (config) {
+    if (config.url.indexOf("/my/userinfo") !== -1) {
+      config.headers.Authorization = localStorage.getItem("token");
+    }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+//添加响应拦截器
+axios.interceptors.request.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
